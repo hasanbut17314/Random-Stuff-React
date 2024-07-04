d12fc9cf252cb47dd86b671c3840b63b6557791b
Hassan Ali <139656401+hasanbut17314@users.noreply.github.com>
2024-07-04 21:13:16 +0500

    nothing

--- src/comp/Facts.jsx
+++ src/comp/Facts.jsx
@@ -1,12 +1,8 @@
 import React, { useState } from 'react'
 
 function Facts() {
-  const [test, setTest] = useState(true)
   return (
-    <div>Facts
-    <button onClick={() => setTest(false)}>Test</button>
-    <p>{test}</p>
-    </div>
+    <div>Facts</div>
   )
 }
 
