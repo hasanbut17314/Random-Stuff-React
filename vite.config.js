5ea32e64215dfd45790b83d60fa59bf98fea8c10
Hassan Ali <139656401+hasanbut17314@users.noreply.github.com>
2024-07-16 23:11:56 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/joke': 'https://v2.jokeapi.dev',
-      '/quotes': 'https://zenquotes.io/api'
-    }
-  },
   plugins: [react()],
 })
